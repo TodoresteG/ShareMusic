9ab8cdb2a7c8832dff673330dd3a5ff7b4415385
Todor Gyuzelev <tgyuzelev+1@gmail.com>
2020-06-12 07:05:26 +0300

    Add partial views for _Layout and add songs page

---
+++ ShareMusic/Controllers/SongsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShareMusic.Controllers
+{
+    public class SongsController : Controller
+    {
+        [Authorize]
+        public IActionResult AddSong() 
+        {
+            return this.View();
+        }
+    }
+}
---
+++ ShareMusic/Models/Songs/AddSongInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShareMusic.Models.Songs
+{
+    public class AddSongInputModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} have to be between {2} and {1} symbols", MinimumLength = 2)]
+        public string Artist { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} have to be between {2} and {1} symbols", MinimumLength = 2)]
+        public string Song { get; set; }
+    }
+}
